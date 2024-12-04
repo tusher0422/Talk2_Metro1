16799658367b6f0aecf31e91bcba43fbef924f78
Nowshin <ibnatnstu@gmail.com>
2024-12-04 19:12:22 +0600

    add stations

--- Metro-Rail-Ticket-System-main/lib/modules/fare_info/controller/fare_info_controller.dart
+++ Metro-Rail-Ticket-System-main/lib/modules/fare_info/controller/fare_info_controller.dart
@@ -12,89 +12,98 @@ class FareInfoController extends GetxController implements GetxService {
     _farecategory = [
       FareInfoCategory(
         image: Images.homelogo,
-        taka: "BDT 50.00",
+        taka: "BDT 20.00",
         start: "Uttara North",
-        des: "Mirpur 10",
+        des: "Uttara Center",
       ),
       FareInfoCategory(
         image: Images.homelogo,
         taka: "BDT 20.00",
-        start: "Dhaka University",
-        des: "Mirpur 10",
-      ),
-      FareInfoCategory(
-        image: Images.homelogo,
-        taka: "BDT 35.00",
-        start: "Uttara South",
-        des: "Mirpur 10",
-      ),
-      FareInfoCategory(
-        image: Images.homelogo,
-        taka: "BDT 60.00",
-        start: "Motijheel",
-        des: "Mirpur 10",
+        start: "Uttara Center",
+        des: "Uttara South",
       ),
       FareInfoCategory(
         image: Images.homelogo,
         taka: "BDT 30.00",
-        start: "Farmgate",
+        start: "Uttara South",
+        des: "Pallabi",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 30.00",
+        start: "Pallabi",
+        des: "Mirpur 11",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 40.00",
+        start: "Mirpur 11",
         des: "Mirpur 10",
       ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 40.00",
+        start: "Mirpur 10",
+        des: "Kazipara",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 50.00",
+        start: "Kazipara",
+        des: "Shewrapara",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 60.00",
+        start: "Shewrapara",
+        des: "Bijoy Sarani",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 70.00",
+        start: "Bijoy Sarani",
+        des: "Farmgate",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 80.00",
+        start: "Farmgate",
+        des: "Karwan Bazar",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 80.00",
+        start: "Karwan Bazar",
+        des: "Shahbag",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 80.00",
+        start: "Shahbag",
+        des: "Dhaka University",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 90.00",
+        start: "Dhaka University",
+        des: "Bangladesh Secretariat",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 100.00",
+        start: "Bangladesh Secretariat",
+        des: "Motijheel",
+      ),
+      FareInfoCategory(
+        image: Images.homelogo,
+        taka: "BDT 100.00",
+        start: "Motijheel",
+        des: "Kamlapur",
+      ),
+
+
+
     ];
     update();
   }
 }
-
-
-// import 'package:get/get.dart';
-// import 'package:mrts/modules/fare_info/model/fare_info_model.dart';
-// import 'package:mrts/utils/images.dart';
-//
-// class FareInfoController extends GetxController implements GetxService{
-//    FareInfoController();
-//    List<FareInfoCategory>? _farecategory;
-//    List<FareInfoCategory>? get farecategory => _farecategory;
-//
-//   Future<void> getData() async{
-//     _farecategory = [
-//       FareInfoCategory(
-//         image: Images.homelogo,
-//         taka: "BDT 50.00",
-//         start: "Uttara North",
-//         des: "Mirpur 10"
-//       ),
-//
-//         FareInfoCategory(
-//         image: Images.homelogo,
-//         taka: "BDT 20.00",
-//         start: "Dhaka University",
-//         des: "Mirpur 10"
-//       ),
-//
-//         FareInfoCategory(
-//         image: Images.homelogo,
-//         taka: "BDT 35.00",
-//         start: "Uttara South",
-//         des: "Mirpur 10"
-//       ),
-//
-//         FareInfoCategory(
-//         image: Images.homelogo,
-//         taka: "BDT 60.00",
-//         start: "Motijheel",
-//         des: "Mirpur 10"
-//       ),
-//
-//         FareInfoCategory(
-//         image: Images.homelogo,
-//         taka: "BDT 30.00",
-//         start: "Farmgate",
-//         des: "Mirpur 10"
-//       ),
-//
-//     ];
-//     update();
-//   }
-//
-//
-// }
